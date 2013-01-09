48daf7c3960dd4688182ff69fe768ffcb66295f7
Danica Dometita <dd492@drexel.edu>
2013-01-09 12:49:03 -0500

    testAlpha

---
+++ lab1F/testAlphaCode.php
@@ -0,0 +1,13 @@
+<?php
+	#Lab 1F
+	#Members: John Carroll, Danica Dometita, Ben Toll, Jarrod Neeser
+	
+	function testAlpha ($str)
+		{
+			if (preg_match('A-Z', $str))
+				return True;
+			else 
+				return False;
+		}
+			
+?>
